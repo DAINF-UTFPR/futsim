e529f6757e668162daf6e8770fd163f584681bd7
Luiz Ribeiro <luizribeiro@gmail.com>
2011-10-05 16:50:40 -0300

    Criado DummyModel e SoccerConstants

---
+++ src/main/java/utfpr/pet/futsim/DummyModel.java
@@ -0,0 +1,27 @@
+package utfpr.pet.futsim;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import utfpr.pet.futsim.entities.Ball;
+import utfpr.pet.futsim.entities.Player;
+import utfpr.pet.futsim.utils.SoccerConstants;
+import utfpr.pet.futsim.utils.Vector2d;
+
+/**
+ * Modelo burro, só para testar o painel de visualização do campo.
+ * 
+ * @author Luiz Ribeiro
+ */
+public class DummyModel implements SoccerModel {
+
+    @Override
+    public Collection<Player> getPlayers() {
+        ArrayList<Player> players = new ArrayList<Player>();
+        return players;
+    }
+
+    @Override
+    public Ball getBall() {
+        return new Ball(new Vector2d(SoccerConstants.FIELD_WIDTH / 2.0, SoccerConstants.FIELD_HEIGHT / 2.0));
+    }
+}
---
+++ src/main/java/utfpr/pet/futsim/utils/SoccerConstants.java
@@ -0,0 +1,14 @@
+package utfpr.pet.futsim.utils;
+
+/*
+ * Classe com várias constantes relativas às partidas da categoria F180.
+ * 
+ * Para informações sobre o tamanho do campo, ver a página de regras disponível
+ * em: {@link http://small-size.informatik.uni-bremen.de/rules:main}.
+ */
+public final class SoccerConstants {
+
+    /** Largura total do campo, incluindo área dos juízes */
+    public static final double FIELD_WIDTH = 7400;
+    public static final double FIELD_HEIGHT = 5400;
+}
